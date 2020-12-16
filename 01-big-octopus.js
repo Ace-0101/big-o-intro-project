afb3b15f16a79a6e8a9325859bb52184dc2b9e9c
Ace Habes <72156174+Ace-0101@users.noreply.github.com>
2020-12-16 14:09:38 -0800

    test

--- 01-big-octopus.js
+++ 01-big-octopus.js
@@ -1,55 +1,62 @@
 const fishies = [
-	'fish',
-	'fiiish',
-	'fiiiiish',
-	'fiiiish',
-	'fffish',
-	'ffiiiiisshh',
-	'fsh',
-	'fiiiissshhhhhh',
-	'fsh',
-	'fsh',
+    'fish',
+    'fiiish',
+    'fiiiiish',
+    'fiiiish',
+    'fffish',
+    'ffiiiiisshh',
+    'fsh',
+    'fiiiissshhhhhh',
+    'fsh',
+    'fsh',
 ]
 let longestFish = quadraticBiggestFish(fishies)
 console.log(longestFish)
 
 function quadraticBiggestFish(fishes) {
-	let longest = ''
-	for (let i = 0; i < fishes.length; i++) {
-		for (let j = 0; j < fishes.length; j++) {
-			let longer = fishes[i].length > fishes[j].length ? fishes[i] : fishes[j]
-			longest = longest.length > longer.length ? longest : longer
-		}
-	}
-	return longest
+    let longest = ''
+    for (let i = 0; i < fishes.length; i++) {
+        for (let j = 0; j < fishes.length; j++) {
+            let longer = fishes[i].length > fishes[j].length ? fishes[i] : fishes[j]
+            longest = longest.length > longer.length ? longest : longer
+        }
+    }
+    return longest
 }
+
 function nlognBiggestFish(fishes) {
-	// Code goes here ...
+    function nlognBiggestFish(fishes) {
+        return fishes.sort((a, b) => a.length - b.length)[fishes.length - 1]
+    }
+
 }
+
 
 function linearBiggestFish(fishes) {
-	// Code goes here ...
+    // Code goes here ...
 }
 
-tilesArray = ['up', 'right-up', 'right', 'right-down', 'down', 'left-down', 'left', 'left-up']
+
+tilesArray = ["up", "right-up", "right", "right-down", "down", "left-down", "left", "left-up"]
 
 function slowDance(direction, tilesArray) {
-	// Code goes here ...
+    // Code goes here ...
 }
 
+
 tilesObj = {
-	up: 0,
-	'right-up': 1,
-	right: 2,
-	'right-down': 3,
-	down: 4,
-	'left-down': 5,
-	left: 6,
-	'left-up': 7,
+    "up": 0,
+    "right-up": 1,
+    "right": 2,
+    "right-down": 3,
+    "down": 4,
+    "left-down": 5,
+    "left": 6,
+    "left-up": 7
 }
 
 function fastDance(direction, tilesObj) {
-	// Code goes here ...
+    // Code goes here ...
 }
 
 //..
