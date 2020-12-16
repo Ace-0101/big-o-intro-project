78a0a6a7c830780bfb8610f6253c4c3f76376690
Ace Habes <72156174+Ace-0101@users.noreply.github.com>
2020-12-16 15:27:26 -0800

    dot

--- 01-big-octopus.js
+++ 01-big-octopus.js
@@ -1,67 +1,67 @@
 function quadraticBiggestFish(fishes) {
-	let longest = ''
-	for (let i = 0; i < fishes.length; i++) {
-		for (let j = 0; j < fishes.length; j++) {
-			let longer = fishes[i].length > fishes[j].length ? fishes[i] : fishes[j]
-			longest = longest.length > longer.length ? longest : longer
-		}
-	}
-	return longest
+    let longest = ''
+    for (let i = 0; i < fishes.length; i++) {
+        for (let j = 0; j < fishes.length; j++) {
+            let longer = fishes[i].length > fishes[j].length ? fishes[i] : fishes[j]
+            longest = longest.length > longer.length ? longest : longer
+        }
+    }
+    return longest
 }
 
 function nlognBiggestFish(fishes) {
-	return fishes.sort((a, b) => a.length - b.length)[fishes.length - 1]
+    return fishes.sort((a, b) => a.length - b.length)[fishes.length - 1]
 }
 
 function linearBiggestFish(fishes) {
-	let longest = ''
-	for (let i = 0; i < fishes.length; i++) {
-		if (fishes[i].length > longest.length) longest = fishes[i]
-		// if (longest.length > fishes[i].length) {
-		//     longest = longest
-		// } else {
-		//     longest = fishes[i].length
-		//*     / This ternary does this ^^^
-		//*     longest = longest.length > fishes[i].length ? longest : fishes[i]
-		// }
-	}
-	return longest
+    let longest = ''
+    for (let i = 0; i < fishes.length; i++) {
+        if (fishes[i].length > longest.length) longest = fishes[i]
+            // if (longest.length > fishes[i].length) {
+            //     longest = longest
+            // } else {
+            //     longest = fishes[i].length
+            //*     / This ternary does this ^^^
+            //*     longest = longest.length > fishes[i].length ? longest : fishes[i]
+            // }.
+    }
+    return longest
 }
 
 tilesArray = ['up', 'right-up', 'right', 'right-down', 'down', 'left-down', 'left', 'left-up']
 
 function slowDance(direction, tilesArray) {
-	// Code goes here ...
+    // Code goes here ...
 }
 
 tilesObj = {
-	up: 0,
-	'right-up': 1,
-	right: 2,
-	'right-down': 3,
-	down: 4,
-	'left-down': 5,
-	left: 6,
-	'left-up': 7,
+    up: 0,
+    'right-up': 1,
+    right: 2,
+    'right-down': 3,
+    down: 4,
+    'left-down': 5,
+    left: 6,
+    'left-up': 7,
 }
 
 function fastDance(direction, tilesObj) {
-	// Code goes here ...
+    // Code goes here ...
 }
 
 //..
 
 const fishies = [
-	'fish',
-	'fiiish',
-	'fiiiiish',
-	'fiiiish',
-	'fffish',
-	'ffiiiiisshh',
-	'fsh',
-	'fiiiissshhhhhh',
-	'fsh',
-	'fsh',
+    'fish',
+    'fiiish',
+    'fiiiiish',
+    'fiiiish',
+    'fffish',
+    'ffiiiiisshh',
+    'fsh',
+    'fiiiissshhhhhh',
+    'fsh',
+    'fsh',
 ]
 let qaudraticFish = quadraticBiggestFish(fishies)
 let nLongnFish = nlognBiggestFish(fishies)
